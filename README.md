b3d37ffb7bb029dae4288187c5dd3738947f6150
maiq <subtlepseudonym@gmail.com>
2016-10-29 14:05:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### Atlas Map Generation Service ###
+
+This repo provides an intranetwork (my home network) REST service for use with <a href="https://github.com/subtlepseudonym/colloid">colloid</a>
+
+#### Requirements ####
+
++ Go
+	- <a href="https://golang.org/doc/install">Install Golang</a>
+
+#### Project Files ####
+
++ atlas.go
+	- On startup, prints the intranetwork ip and port that it's listening on
+	- TODO: Map generation
+	- TODO: swagger for API endpoints (and update of /)
---
+++ atlas.go
@@ -0,0 +1,71 @@
+/* Map Generator
+ * subtlepseudonym
+ */
+
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var (
+	lasPort string = ":10010"
+	routerIP string = "192.168.1.1"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// Index (currently) pulls boostrap from maxcdn rather than local storage
+	http.ServeFile(w, r, "index.json")
+}
+
+func getIP() (string, error) {
+	// Queries router for intranetwork ip address then pipes it to awk
+	ip := exec.Command("ip", "route", "get", routerIP)
+	awk := exec.Command("awk", "{ print $NF; exit }")
+
+	// Connecting the pipe
+	reader, writer := io.Pipe()
+	var buf bytes.Buffer
+	ip.Stdout = writer
+	awk.Stdin = reader
+	awk.Stdout = &buf
+
+	ip.Start()
+	awk.Start()
+
+	ip.Wait()
+	writer.Close()
+
+	awk.Wait()
+	reader.Close()
+
+	ret := strings.TrimSpace(buf.String())
+	if ret == "" {
+		return "", errors.New("Error retrieving intranetwork ip from " + routerIP)
+	}
+
+	return ret, nil
+}
+
+func init() {
+	// Enable logging to local file
+	log.SetOutput(os.Stdout)
+
+	// Print public ip address and ListenAndServe port
+	pub_ip, err := getIP()
+	if err != nil {	log.Fatal(err) }
+	log.Println("ListenAndServe at", pub_ip+lasPort)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	log.Fatal(http.ListenAndServe(lasPort, nil))
+}
